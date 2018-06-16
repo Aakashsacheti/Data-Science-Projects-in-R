ef5915b18e6fcc8e40e3e71c768c1fa28d7810be
Aakash Sacheti <38552958+Aakashsacheti@users.noreply.github.com>
2018-06-16 08:31:00 +0530

    Update decisionTreeLetters.R

--- Decision trees (Random Forest methodology)/decisionTreeLetters.R
+++ Decision trees (Random Forest methodology)/decisionTreeLetters.R
@@ -1,7 +1,4 @@
-#Problem Statement
-#In this problem, you will build a Decision Tree model that uses statistics of images 
-#of four letters in the Roman alphabet -- A, B, P, and R -- to predict which letter 
-#a particular image corresponds to. 
+
 
 #Application of CART and Random Forests
 
